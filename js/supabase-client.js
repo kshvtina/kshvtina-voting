f683f514eda9c0a6e67c0246e3fbc8a8b7785969
kshvtina <kshvtina@gmail.com>
2026-06-21 18:57:45 +0700

    Add files via upload

---
+++ js/supabase-client.js
@@ -0,0 +1,7 @@
+<script>
+// Ganti dengan punya kamu
+const SUPABASE_URL = 'https://tyrjphwdneahqjwqszuo.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5cmpwaHdkbmVhaHFqd3FzenVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MTAyNTAsImV4cCI6MjA5NzA4NjI1MH0.9qlqiyAALvOznLJ4Yq8VMPnLvvyW6CbOKlin6DrqQvs';
+
+const supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+</script>
